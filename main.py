83e63dd27eb8663af9f0c6732f1ea446249ce002
Yhuanhuan01 <118103563+Yhuanhuan01@users.noreply.github.com>
2024-07-29 13:13:26 +0800

    Update main.py
    Add main.py error reporting echo

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-'''
-test
-'''
 import sys
 import ld_patch
 
-libc = sys.argv[1]
-elf = sys.argv[2]
 
-if not sys.argv[1]:
-    print("Not Found libc")
-    exit(0)
 
-if not sys.argv[2]:
-    print("Not Found elf")
+def help():
+    print("[-] Usage: python main.py <libcname> <elfname>")
     exit(0)
+    
+def check():
+    if len(sys.argv) < 2 or not sys.argv[1]:
+        print("Not Found libc")
+        help()
+    if len(sys.argv) < 3 or not sys.argv[2]:
+        print("Not Found elf")
+        help()
 
 
 if __name__ == '__main__':
+    check()
+    libc = sys.argv[1]
+    elf = sys.argv[2]
     match_libc = ld_patch.Found(libc,elf)
     ld_patch.download(match_libc)
     ld_patch.patchelf(elf)
